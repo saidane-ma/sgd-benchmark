d95b4af66ab658c3e16cab167258d77b1935a579
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-12 14:38:07 +0100

    Adagrad implementation

---
+++ optimizers/adagrad.py
@@ -0,0 +1,16 @@
+import numpy as np
+from problems.logistics import *
+class Adagrad:
+    def __init__(self,X,y,w):
+        self.X=X
+        self.y=y
+        self.L=LogisticRegression(self.X,self.y)
+        self.g=np.zeros_like(w)
+
+    def adagrad(self,w,indices,alpha=0.1):
+        self.g+=np.square(self.L.gradient(w,indices))
+        w=w-(alpha/(np.sqrt(self.g)))*self.L.gradient(w,indices)
+        
+        return w
+
+    
