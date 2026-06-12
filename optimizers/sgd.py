ebf43fb02b078f227ef08b73a5f0a8ce7fd12c08
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-12 11:19:46 +0100

    Implemented standard momentumSGD + Polyak variation + Nestrov variation

---
+++ optimizers/sgd.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from problems.logistics import *
+
+class SGD:
+    def __init__(self,X,y):
+        self.X=X
+        self.y=y
+    
+    def polyak(self,w,beta=0.9):
+        L=LogisticRegression(self.X,self.y)
+        v=np.zeros_like(w)
+        for i in range(len(self.X)):
+            v=beta*v+(1-beta)*L.gradient(w)
+            w-=v
+        return w
+    
+    def momentum(self,w,beta=0.9):
+        L=LogisticRegression(self.X,self.y)
+        v=np.zeros_like(w)
+        for i in range(len(self.X)):
+            v=beta*v+L.gradient(w)
+            w-=(1-beta)*v
+        return w
+
+    def nestrov(self,w,beta=0.9):
+        L=LogisticRegression(self.X,self.y)
+        v=np.zeros_like(w)
+        for i in range(len(self.X)):
+            v=beta*v+(1-beta)*L.gradient(w-v)
+            w-=v
+        return v
+        
