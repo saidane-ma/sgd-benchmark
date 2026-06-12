89d2aba24f88db4f04c3d8dde8749284c1e368a1
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-12 12:41:32 +0100

    Bug fixes and variable separation

--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -3,31 +3,28 @@ import numpy as np
 from problems.logistics import *
 
 class SGD:
-    def __init__(self,X,y):
+    def __init__(self,X,y,w):
         self.X=X
         self.y=y
+        self.v_polyak=np.zeros_like(w)
+        self.v_nestrov=np.zeros_like(w)
+        self.v_momentum=np.zeros_like(w)
     
-    def polyak(self,w,indices,beta=0.9):
+    def polyak(self,w,indices,alpha=0.1,beta=0.9):
         L=LogisticRegression(self.X,self.y)
-        v=np.zeros_like(w)
-        for i in range(len(self.X)):
-            v=beta*v+(1-beta)*L.gradient(w,indices)
-            w-=v
+        self.v_polyak=beta*self.v_polyak+alpha*L.gradient(w,indices)
+        w-=self.v_polyak
         return w
     
-    def momentum(self,w,indices,beta=0.9):
+    def momentum(self,w,indices,alpha=0.1,beta=0.9):
         L=LogisticRegression(self.X,self.y)
-        v=np.zeros_like(w)
-        for i in range(len(self.X)):
-            v=beta*v+L.gradient(w,indices)
-            w-=(1-beta)*v
+        self.v_momentum=beta*self.v_momentum+L.gradient(w,indices)
+        w-=alpha*self.v_momentum
         return w
 
-    def nestrov(self,w,indices,beta=0.9):
+    def nestrov(self,w,indices,alpha=0.1,beta=0.99):
         L=LogisticRegression(self.X,self.y)
-        v=np.zeros_like(w)
-        for i in range(len(self.X)):
-            v=beta*v+(1-beta)*L.gradient(w-v,indices)
-            w-=v
-        return v
+        self.v_nestrov=beta*self.v_nestrov+alpha*L.gradient(w-beta*self.v_nestrov,indices)
+        w-=self.v_nestrov
+        return w
         
