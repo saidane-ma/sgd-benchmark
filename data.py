e0c4f4af94c3b924398417b27d5d751ed78bc450
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-13 22:41:17 +0100

    Added another Gaussian Blob to simulate two classes linearly separable

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generate_gaussian_blobs(n_features=2, n_samples=1000, random_state_seed=5):
+def generate_gaussian_blobs(n_features=2, n_samples=1000, random_state_seed=5,factor=0.5,center=3.0):
     """
     Generates linearly separable Gaussian blobs for binary classification.
     Returns:
@@ -9,10 +9,22 @@ def generate_gaussian_blobs(n_features=2, n_samples=1000, random_state_seed=5):
     """
 
     np.random.seed(random_state_seed)
+    n_class=int(n_samples*factor)
+
     b=np.array([1]*n_samples)
-    X=np.random.randn(n_samples,n_features)
-    X=np.insert(X,0,b,axis=1)
-    y=np.random.randint(0,2,size=(n_samples,))
+    X0=np.random.randn(n_class,n_features)-center
+    y0=np.zeros(n_class)
+
+    X1=np.random.randn(n_samples-n_class,n_features)+center
+    y1=np.ones(n_samples-n_class)
+
+    X = np.vstack((X0, X1))
+    y = np.concatenate((y0, y1))
+    X = np.insert(X, 0, b, axis=1)
+    
+    indices = np.random.permutation(n_samples)
+    X = X[indices]
+    y = y[indices]
 
     return X,y
 
