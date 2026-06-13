22f984ee206ed310bdde770e08fc12ee2d19eb44
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-13 10:26:52 +0100

    SAG implementation

---
+++ optimizers/sag.py
@@ -0,0 +1,17 @@
+import numpy as np
+from problems.logistics import *
+
+np.random.seed(5)
+
+class SAG:
+    def __init__(self,X,y,w):
+        self.X=X
+        self.y=y
+        self.L=LogisticRegression(X,y)
+        self.g=np.zeros_like(self.X)
+
+    def sag(self,w,indices,alpha=0.0001):
+        k=np.random.choice(indices)
+        self.g[k-1]=self.L.gradient(w,[k])
+        w-=alpha*np.mean(self.g,axis=0)
+        return w
