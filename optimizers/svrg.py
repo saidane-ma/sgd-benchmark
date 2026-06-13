0528f8a1eb09a64597cd91cb3aba97ae35cdfcef
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-13 23:09:11 +0100

    SVRG bug fixing with array instancing

--- optimizers/svrg.py
+++ optimizers/svrg.py
@@ -7,11 +7,13 @@ class SVRG:
         self.y=y
         self.L=LogisticRegression(X,y)
         
-    def svrg(self,w,indices,alpha=0.01):
-        w_tilde=w
+    def svrg(self,w,indices,alpha=0.001):
+        w_tilde=w.copy()
         mu=self.L.gradient(w_tilde)
         for i in range(2*len(self.X)):
-            k= np.random.choice(indices)
+            k= np.random.choice(len(self.X))
             g=self.L.gradient(w,[k])-self.L.gradient(w_tilde,[k])+mu
             w-= g*alpha
-        return w
+        return w 
+    def step(self,w,indices,i,alpha=0.001):
+        return self.svrg(w,indices,alpha)
