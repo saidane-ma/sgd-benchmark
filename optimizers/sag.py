bf15b1dd21954ae19dbc536490acc624068d45ec
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-14 16:33:10 +0100

    Bug fixes and initialization problem resolved

--- optimizers/sag.py
+++ optimizers/sag.py
@@ -8,12 +8,14 @@ class SAG:
         self.X=X
         self.y=y
         self.L=LogisticRegression(X,y)
-        self.g=np.zeros_like(self.X)
+        self.g=np.zeros((len(self.X),len(w)))
+        for i in range(len(self.X)):
+            self.g[i]=self.L.gradient(w, [i])
 
-    def sag(self,w,indices,alpha=0.1):
-        k=np.random.choice(indices)
-        self.g[k-1]=self.L.gradient(w,[k])
+    def sag(self,w,indices,alpha=0.003):
+        k=np.random.choice(len(self.X))
+        self.g[k]=self.L.gradient(w,[k])
         w-=alpha*np.mean(self.g,axis=0)
         return w
-    def step(self,w,indices,i,alpha=0.1):
+    def step(self,w,indices,i,alpha=0.003):
         return self.sag(w,indices,alpha)
