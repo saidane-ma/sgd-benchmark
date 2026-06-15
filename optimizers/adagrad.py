69269177934dbe7016aa40a728b9de94f1b06973
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-15 17:30:19 +0100

    Restructuring and ghost variable removal

--- optimizers/adagrad.py
+++ optimizers/adagrad.py
@@ -1,18 +1,17 @@
 import numpy as np
 from problems.logistics import *
+
 class Adagrad:
-    def __init__(self,X,y,w):
-        self.X=X
-        self.y=y
-        self.L=LogisticRegression(self.X,self.y)
+    def __init__(self,w,problem):
+        self.problem=problem
         self.g=np.zeros_like(w)
 
     def adagrad(self,w,indices,alpha=0.1):
-        self.g+=np.square(self.L.gradient(w,indices))
-        w=w-(alpha/(np.sqrt(self.g + 1e-10)))*self.L.gradient(w,indices)
+        self.g+=np.square(self.problem.gradient(w,indices))
+        w=w-(alpha/(np.sqrt(self.g + 1e-10)))*self.problem.gradient(w,indices)
         
         return w
 
-    def step(self,w,indices,i,alpha=0.1):
+    def step(self,w,indices,i,alpha=0.1,**kwargs):
         return self.adagrad(w,indices,alpha)
     
