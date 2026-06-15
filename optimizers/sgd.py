4fcd66c00cfa5ca54673029c22dda614d6122d27
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-15 17:30:35 +0100

    Restructuring and ghost variable removal

--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -1,36 +1,31 @@
 import numpy as np
 
-from problems.logistics import *
+from problems import *
 
 class SGD:
-    def __init__(self,X,y,w,mode):
-        self.X=X
-        self.y=y
+    def __init__(self,problem,mode,w):
         self.v_polyak=np.zeros_like(w)
         self.v_nestrov=np.zeros_like(w)
         self.v_momentum=np.zeros_like(w)
-        self.L=LogisticRegression(self.X,self.y)
+        self.problem=problem
         self.mode=mode
     
     def polyak(self,w,indices,alpha=0.1,beta=0.9):
-        L=LogisticRegression(self.X,self.y)
-        self.v_polyak=beta*self.v_polyak+alpha*L.gradient(w,indices)
+        self.v_polyak=beta*self.v_polyak+alpha*self.problem.gradient(w,indices)
         w-=self.v_polyak
         return w
     
     def momentum(self,w,indices,alpha=0.1,beta=0.9):
-        L=LogisticRegression(self.X,self.y)
-        self.v_momentum=beta*self.v_momentum+L.gradient(w,indices)
+        self.v_momentum=beta*self.v_momentum+self.problem.gradient(w,indices)
         w-=alpha*self.v_momentum
         return w
 
     def nestrov(self,w,indices,alpha=0.1,beta=0.99):
-        L=LogisticRegression(self.X,self.y)
-        self.v_nestrov=beta*self.v_nestrov+alpha*L.gradient(w-beta*self.v_nestrov,indices)
+        self.v_nestrov=beta*self.v_nestrov+alpha*self.problem.gradient(w-beta*self.v_nestrov,indices)
         w-=self.v_nestrov
         return w
     
-    def step(self,w,indices,i,alpha=0.001,beta1=0.9,beta2=0.99):
+    def step(self,w,indices,i,alpha=0.001,beta1=0.9,beta2=0.99,**kwargs):
         if self.mode=="polyak": return self.polyak(w,indices,alpha,beta1)
         elif self.mode=="nesterov" : return self.nestrov(w,indices,0.001,beta2)
-        else : return w - 0.01*self.L.gradient(w,indices)
+        else : return w - alpha*self.problem.gradient(w,indices)
