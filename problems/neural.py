430db4a98669a4290bb9f27d2c571af20a335f05
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-16 15:37:52 +0100

    Restructing for the newest benchmark structure

--- problems/neural.py
+++ problems/neural.py
@@ -2,16 +2,23 @@ import numpy as np
 from problems.logistics import *
 
 class NeuralNet:
-    def __init__(self,X,y):
+    def __init__(self,X,y,hidden=4,activation="sigmoid"):
         self.X=X
         self.y=y
         self.L=LogisticRegression(X,y)
+        self.shape= (X.shape[1],hidden)
+        self.shape_= (hidden+1,1)
+        self.activation={
+            "sigmoid" : self.sigmoid_NN,
+            "ReLU"  : self.ReLU_NN,
+            "tanh"  : self.tanh_NN,      
+        }[activation]
 
     def flatten(self,w,w_):
         return np.concatenate([w.flatten(),w_.flatten()])
     
-    def unflatten(self,w,shape,shape_):
-        return w[:(shape[0]*shape[1])].reshape(shape), w[(shape[0]*shape[1]):].reshape(shape_)
+    def unflatten(self,w):
+        return w[:(self.shape[0]*self.shape[1])].reshape(self.shape), w[(self.shape[0]*self.shape[1]):].reshape(self.shape_)
     
     def sigmoid_NN(self,w,w_):
         z=self.X@w
@@ -32,7 +39,7 @@ class NeuralNet:
         b=np.array([1]*len(y1))
         y1_=np.insert(y1,0,b,axis=1)
 
-        y_pred=self.L.sigmoid(y1@w_) #necessary for classification problems
+        y_pred=self.L.sigmoid(y1_@w_) #necessary for classification problems
         err=(y_pred-self.y.reshape(-1,1))/len(self.y) #normalization 
 
         grad_=np.transpose(y1_)@err
@@ -46,7 +53,7 @@ class NeuralNet:
         y1=np.tanh(z)
         b=np.array([1]*len(y1))
         y1_=np.insert(y1,0,b,axis=1)
-        y_pred=self.L.sigmoid(y1@w_) #necessary for classification problems
+        y_pred=self.L.sigmoid(y1_@w_) #necessary for classification problems
 
         err=(y_pred-self.y.reshape(-1,1))/len(self.y) #normalization 
 
@@ -55,7 +62,16 @@ class NeuralNet:
         grad=np.transpose(self.X)@err
         return grad, grad_,y_pred
     
-    def loss(self,y_pred):
-        n=len(self.X)
-        s=-self.y*np.log(y_pred+(1e-10))-(1-self.y)*np.log(1-y_pred+(1e-10))
+    def loss(self,w):
+        w1,w2=self.unflatten(w)
+        _,_,y_pred=self.activation(w1,w2)
+        y_pred=y_pred.flatten()
+        n= len(self.X)
+        s= -self.y*np.log(y_pred+1e-10)-(1-self.y)*np.log(1-y_pred+1e-10)
         return np.sum(s)/n
+    
+    def gradient(self,w,indices=None):
+        w1 , w2= self.unflatten(w)
+        gw1 , gw2,_=self.activation(w1,w2)
+        return self.flatten(gw1,gw2)
+    
