07ae6d1b8dcccee9cccd30bce5809680dd0c9613
saidane-ma <mohamedamine.saidane@etudiant-enit.utm.tn>
2026-06-16 15:39:20 +0100

    ghost variable removal and stability improv

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -7,7 +7,7 @@ class Adam:
         self.m=np.zeros_like(w)
         self.m2=np.zeros_like(w)
         
-    def adam(self,w,indices,n,beta1=0.9,beta2=0.999,alpha=0.01,grad=None):
+    def adam(self,w,indices,n,beta1=0.9,beta2=0.999,alpha=0.01):
         g=self.problem.gradient(w,indices)
         self.m=self.m*beta1 + (1-beta1)*g
         self.m2=self.m2*beta2 + (1-beta2)*g*g
@@ -15,7 +15,7 @@ class Adam:
         # bias correction
         m_bias=self.m/(1-beta1**n)
         m2_bias=self.m2/(1-beta2**n)
-        w-= alpha/(np.sqrt(m2_bias)+1e-10)*m_bias
+        w-= alpha/(np.sqrt(m2_bias)+1e-6)*m_bias
 
         return w
     
